52ca7f60edce6bb2cb4efab3aafb7ac98cb343f5
„Szymon <szymonk@onet.pl>
2020-11-23 12:18:37 +0100

    Zakończono zadanie 2.2.3

--- Zadanie-2-2/src/main.cpp
+++ Zadanie-2-2/src/main.cpp
@@ -1,13 +1,37 @@
 #include <Arduino.h>
-#define LED_BLUE 10
-void setup() {
-pinMode(LED_BUILTIN, OUTPUT);
-digitalWrite(LED_BLUE, LOW);
+
+#define LED_RED 10
+
+#define M_KROPKA_T 200
+#define M_KRESKA_T 3 * M_KROPKA_T
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
+
+void setup()
+{
+  pinMode(LED_RED, OUTPUT);
 }
 
-void loop() {
-digitalWrite(LED_BLUE, HIGH);
-delay(500);
-digitalWrite(LED_BLUE, LOW);
-delay(500);
+void loop()
+{
+  for (int i = 0; i < 3; i++){
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KRESKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  for (int i=0; i < 3; i++)
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KRESKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  for (int i = 0; i < 3; i++){
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KRESKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  delay(M_SPACJA_T);
 }
